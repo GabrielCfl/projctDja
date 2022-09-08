d3d4bea119f904f8c931e1ae1f09c30973e14acf
GabrielCfl <gabrielchrf@gmail.com>
2022-09-08 17:28:18 -0400

    task

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,12 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+
 def helloworld(request):
     return HTTPResponse('Hello World')
+
+def tasklist(request):
+    return render(request, 'task/list.html')
+
 
 # Create your views here.
